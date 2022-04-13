ff0806880ef3e7ee3bdd42d0da62a4bb5dba6d22
MAK|MAS|MP <87887719+IRLLYYYDONO@users.noreply.github.com>
2022-04-13 19:32:33 +0700

    fix everything except that part where I cant jump over one block

--- main.ts
+++ main.ts
@@ -11,16 +11,16 @@ let level_one = images.createBigImage(`
 `)
 
 // Music array
-let notes = ["c4:1", "e", "g", "c5", "e5", "g4", "c5", "e5", "c4", "e", "g", "c5", "e5", "g4", "c5", "e5", "c4", 
-            "d", "a", "d5", "f5", "a4", "d5", "f5", "c4", "d", "a", "d5", "f5", "a4", "d5", "f5", "b3", "d4", "g  ",
-            "d5", "f5", "g4", "d5", "f5", "b3", "d4", "g", "d5", "f5", "g4", "d5", "f5", "c4", "e", "g", "c5", "e5",
-            "g4", "c5", "e5", "c4", "e", "g", "c5", "e5", "g4", "c5", "e5", "c4", "e", "a", "e5", "a5", "a4", "e5",
-            "a5", "c4", "e", "a", "e5", "a5", "a4", "e5", "a5", "c4", "d", "f#", "a", "d5", "f#4", "a", "d5", "c4",
-            "d", "f#", "a", "d5", "f#4", "a", "d5", "b3", "d4", "g", "d5", "g5", "g4", "d5", "g5", "b3", "d4", "g",
-            "d5", "g5", "g4", "d5", "g5", "b3", "c4", "e", "g", "c5", "e4", "g", "c5", "b3", "c4", "e", "g", "c5",
-            "e4", "g", "c5", "a3", "c4", "e", "g", "c5", "e4", "g", "c5", "a3", "c4", "e", "g", "c5", "e4", "g",
-            "c5", "d3", "a", "d4", "f#", "c5", "d4", "f#", "c5", "d3", "a", "d4", "f#", "c5", "d4", "f#", "c5", "g3",
-            "b", "d4", "g", "b", "d", "g", "b", "g3", "b3", "d4", "g", "b", "d", "g", "b"]
+let notes = ["c4:1", "e", "g", "c5", "e5", "g4", "c5", "e5", "c4", "e", "g", "c5", "e5", "g4", "c5", "e5", "c4",
+    "d", "a", "d5", "f5", "a4", "d5", "f5", "c4", "d", "a", "d5", "f5", "a4", "d5", "f5", "b3", "d4", "g  ",
+    "d5", "f5", "g4", "d5", "f5", "b3", "d4", "g", "d5", "f5", "g4", "d5", "f5", "c4", "e", "g", "c5", "e5",
+    "g4", "c5", "e5", "c4", "e", "g", "c5", "e5", "g4", "c5", "e5", "c4", "e", "a", "e5", "a5", "a4", "e5",
+    "a5", "c4", "e", "a", "e5", "a5", "a4", "e5", "a5", "c4", "d", "f#", "a", "d5", "f#4", "a", "d5", "c4",
+    "d", "f#", "a", "d5", "f#4", "a", "d5", "b3", "d4", "g", "d5", "g5", "g4", "d5", "g5", "b3", "d4", "g",
+    "d5", "g5", "g4", "d5", "g5", "b3", "c4", "e", "g", "c5", "e4", "g", "c5", "b3", "c4", "e", "g", "c5",
+    "e4", "g", "c5", "a3", "c4", "e", "g", "c5", "e4", "g", "c5", "a3", "c4", "e", "g", "c5", "e4", "g",
+    "c5", "d3", "a", "d4", "f#", "c5", "d4", "f#", "c5", "d3", "a", "d4", "f#", "c5", "d4", "f#", "c5", "g3",
+    "b", "d4", "g", "b", "d", "g", "b", "g3", "b3", "d4", "g", "b", "d", "g", "b"]
 
 // Veriables
 let xOffset = -2
@@ -64,7 +64,7 @@ while (true) {
         playerGravity_yOffset()
         renderAll()
     }
-    
+
     // input for button B for jumping
     if (input.buttonIsPressed(Button.B)) {
         goingToJump()
@@ -99,52 +99,54 @@ function player_movement(button: string) {
 // dependding on wether we are on the ground or not, this is also intigrated with
 // the jumping function
 function playerGravity_yOffset() {
-    
+
     // this here is important because one this if statment fix all the problems
     // that is connected to the jumping machanic and gravity mechanic, you see
     // without this checking 
     if (onGround == false && isJumping == false && jumpStateObjectCheck < 1) {
         player_yOffset += 1
-    } else{
+    } else {
         return
     }
-        
+
 }
 
 // this is resposible for all the collision dectection befor the movement
 // function 
 function playerCollision() {
     // giving permission
-        
+
     // player collision check forward
     player_forwards_partone = led.pointBrightness(3, player_yOffset)
     player_forwards_parttwo = led.pointBrightness(3, player_yOffset + 1)
-    
+
     // player collision check backwards
     player_backwards_partone = led.pointBrightness(1, player_yOffset)
     player_backwards_parttwo = led.pointBrightness(1, player_yOffset + 1)
-    
+
     // player collision check upwards
     player_above = led.pointBrightness(2, player_yOffset - 1)
-    
+
     // player collision check downwards
     player_downwards = led.pointBrightness(2, player_yOffset + 2)
 
     // check object in the way of the jumping trajectory 
     jumpStateObjectCheck = led.pointBrightness(3, player_yOffset + 2)
-    
+
     // configuring the onGround Veriables
     if (player_downwards < 1) {
         onGround = false
     } else if (player_downwards > 1) {
         onGround = true
     }
-        
+
 }
 
 // responsible for rendering the player and the map
 function renderAll() {
+
     level_one.showImage(xOffset, player_Speed)
+
     led.plot(2, player_yOffset)
     led.plot(2, player_yOffset + 1)
 }
